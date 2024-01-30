641fcc19d8cdb46f95b42834881d7d7de4d7b9a9
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-01-30 13:06:54 +0100

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasa_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hrvatska ima more";
+            string s2 = "Italija je prvak svijeta";
+            string s3 = s1.Substring(0, 7) + s2.Substring(6);
+
+            Console.WriteLine(s3);
+
+            Console.ReadLine();
+        }
+    }
+}
